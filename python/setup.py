1eee968c3ce1f2565de533a884fcae2e26a08f54
Mohammed Aljaroudi <maljaroudi@sandiego.edu>
2021-05-05 01:49:53 -0700

    Init

---
+++ python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='mochio',
+    packages=find_packages(include=['mochio']),
+    version='1.0.0',
+    description='A general-purpose library',
+    author='Mohammed Aljaroudi',
+    license='MIT',
+    install_requires=[]
+)
